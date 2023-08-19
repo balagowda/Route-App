cb83bfc51b28f2669eed9ae646e901b0dbc5e162
balagowda <balagowda9483@gmail.com>
2023-08-19 21:15:22 +0530

    navbar design and homepage decoration

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Avatar from '@mui/material/Avatar';
+import { green } from "@mui/material/colors";
 
 const NavBar = () => {
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+        <Navbar.Brand href="/">MyRouter</Navbar.Brand>
+        <Nav className="ms-auto">
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>&nbsp;hadz
+          <Avatar sx={{ bgcolor: green }}>BP</Avatar>
+        </Nav>
       </Container>
     </Navbar>
   );
